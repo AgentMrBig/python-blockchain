573e08afe40eef9c2456f77c4095686a6a171ece
AgentMrBig <agentmrbig@gmail.com>
2020-10-01 00:17:03 -0400

    sperated block class from blockchain into its own module

--- blockchain.py
+++ blockchain.py
@@ -1,25 +1,25 @@
-class Block:
-    """
-    Block: a unit of storage.
-    Store transactions in a blockchain that supports a cryptocurrency.
-    """
-    def __init__(self, data):
-        self.data = data
+from block import Block
+
 
 class Blockchain:
-    """ 
+    """
     Blackchain: a public ledger of transactions.
     Implemented as a list of blocks - data sets of transations
     """
+
     def __init__(self):
         self.chain = []
 
     def add_block(self, data):
         self.chain.append(Block(data))
 
+    def __repr__(self):
+        return f'Blockchain: {self.chain}'
+
+
 blockchain = Blockchain()
 blockchain.add_block('one')
 blockchain.add_block('two')
-blockchain.add_block('three')
+
 
 print(blockchain)

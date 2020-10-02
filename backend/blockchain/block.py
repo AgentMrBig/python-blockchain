d490bdc7037c3c0eb7046ceddbf16e4326921cff
AgentMrBig <agentmrbig@gmail.com>
2020-10-02 01:42:31 -0400

    added config.py containing definitions for various time values. added adjusting difficulty for mined blocks, if too difficult decrease difficulty if too easy increase difficulty, added tests for adjusting difficutly code

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,7 @@
 import time
 
 from backend.util.crypto_hash import crypto_hash
+from backend.config import MINE_RATE
 
 GENESIS_DATA = {
     'timestamp': 1,
@@ -45,7 +46,7 @@ class Block:
         """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        difficulty = last_block.difficulty
+        difficulty = Block.adjust_difficulty(last_block, timestamp)
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
@@ -63,6 +64,22 @@ class Block:
         """
 
         return Block(**GENESIS_DATA)
+
+    @staticmethod
+    def adjust_difficulty(last_block, new_timestamp):
+        """
+        Calculate the adjusted difficulty according to the MINE_RATE
+        Increase the difficulty for quickly minded blocks.
+        Decrease the difficulty for slowly mined blocks.
+        """
+
+        if(new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
+
+        if(last_block.difficulty - 1) > 0:
+            return last_block.difficulty - 1
+
+        return 1
 
 
 def main():
